5e02654417f82a5beaaf6cb094c9a4a4c14c3fa6
ReyhanYulanda <arimcoding@gmail.com>
2026-02-03 20:17:30 +0800

    optimalize(image reduce processing): optimalize function code of image reduce processing

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -2,41 +2,85 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Storage;
-
 class ImageService
 {
-    public function compressAndSave($photo, $filename, $quality = 50)
+    public function compressAndSave($photo, $filename, $quality = 70)
     {
-        $path = storage_path('app/public/absensi_photos/'.$filename);
-
-        if ($photo->getSize() > 2 * 1024 * 1024) {
-            $this->compressImage($photo->getRealPath(), $path, $quality);
-        } else {
-            $photo->move(storage_path('app/public/absensi_photos'), $filename);
+        $dir = storage_path('app/public/absensi_photos');
+        if (!is_dir($dir)) {
+            mkdir($dir, 0755, true);
         }
 
-        return 'absensi_photos/'.$filename;
+        $destinationPath = $dir . '/' . $filename;
+
+        if ($photo->getSize() < 1024 * 1024) {
+            $photo->move($dir, $filename);
+            return 'absensi_photos/' . $filename;
+        }
+
+        $this->resizeAndCompress(
+            $photo->getRealPath(),
+            $destinationPath,
+            $quality
+        );
+
+        return 'absensi_photos/' . $filename;
     }
-    
-    private function compressImage($sourcePath, $destinationPath, $quality = 50)
+
+    private function resizeAndCompress($sourcePath, $destinationPath, $quality)
     {
         $info = getimagesize($sourcePath);
-
-        if ($info['mime'] == 'image/jpeg') {
-            $image = imagecreatefromjpeg($sourcePath);
-            imagejpeg($image, $destinationPath, $quality);
-        } elseif ($info['mime'] == 'image/png') {
-            $image = imagecreatefrompng($sourcePath);
-            imagejpeg($image, $destinationPath, $quality);
-        } elseif ($info['mime'] == 'image/webp') {
-            $image = imagecreatefromwebp($sourcePath);
-            imagewebp($image, $destinationPath, $quality);
-        } else {
+        if (!$info) {
             copy($sourcePath, $destinationPath);
+            return;
         }
 
-        imagedestroy($image);
-    }
+        [$width, $height] = $info;
+        $mime = $info['mime'];
 
+        $maxWidth = 1280;
+        if ($width > $maxWidth) {
+            $ratio = $maxWidth / $width;
+            $newWidth = $maxWidth;
+            $newHeight = (int) ($height * $ratio);
+        } else {
+            $newWidth = $width;
+            $newHeight = $height;
+        }
+
+        switch ($mime) {
+            case 'image/jpeg':
+                $src = imagecreatefromjpeg($sourcePath);
+                break;
+            case 'image/png':
+                $src = imagecreatefrompng($sourcePath);
+                break;
+            case 'image/webp':
+                $src = imagecreatefromwebp($sourcePath);
+                break;
+            default:
+                copy($sourcePath, $destinationPath);
+                return;
+        }
+
+        $dst = imagecreatetruecolor($newWidth, $newHeight);
+
+        imagecopyresampled(
+            $dst,
+            $src,
+            0,
+            0,
+            0,
+            0,
+            $newWidth,
+            $newHeight,
+            $width,
+            $height
+        );
+
+        imagejpeg($dst, $destinationPath, $quality);
+
+        imagedestroy($src);
+        imagedestroy($dst);
+    }
 }
